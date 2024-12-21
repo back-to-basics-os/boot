d5278a952d5ccaa07a0af7c27bc2178044f1cfee
DaemonMCR <thomas_turner@talktalk.net>
2024-12-21 22:11:24 +0000

    boot: Print to tty

--- x86_primary_boot_sector.asm
+++ x86_primary_boot_sector.asm
@@ -1,6 +1,31 @@
 ; Infinite loop (e9 fd ff)
-loop:
-    jmp loop
+
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; 'l' is still on al, remember?
+mov al, 'o'
+int 0x10
+mov al, ''
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
+mov al, '!'
+int 0x10
+
+jmp $ ; jump to current address = infinite loop
 
 ; Write 510 bytes of blank memory
 times 510-($-$$) db 0
