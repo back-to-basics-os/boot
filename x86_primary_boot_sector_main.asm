7665c132a2f5ed6871dd4cff58fe1307c0f0929d
DaemonMCR <thomas_turner@talktalk.net>
2024-12-26 12:55:54 +0000

    boot: cleanup

--- x86_primary_boot_sector_main.asm
+++ x86_primary_boot_sector_main.asm
@@ -1,4 +1,5 @@
 [org 0x7c00]
+
 mov bx, HELLO
 call print
 call print_nl
@@ -8,42 +9,18 @@ call print
 mov bx, 0 ; Clear bx register so we don't get any unexpected output
 call print_nl
 
+mov dx, 0x12fe
+call print_hex
+
 HELLO:
     db 'Hello World!', 0
 
 GOODBYE:
     db 'Goodbye!', 0
 
-print:
-    pusha
-    jmp print_logic
-
-print_logic:
-    mov al, [bx]
-    cmp al, 0
-    je cleanup
-
-    mov ah, 0x0e
-    int 0x10
-    add bx, 1
-    jmp print_logic
-
-cleanup:
-    popa
-    ret
-
-print_nl:
-    pusha
-    mov ah, 0x0e
-    mov al, 0x0a
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-
-    popa
-    ret
-
 jmp $ ; infinite loop
+
+%include "boot/x86_primary_boot_sector_print.asm"
 
 ; zero padding and magic bios number
 times 510-($-$$) db 0
